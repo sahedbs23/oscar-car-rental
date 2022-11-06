27d9f69062b3306020cc3334f047c8ce611a1fec
sawon <sawon@sawons-MacBook-Pro.local>
2022-11-07 01:41:25 +0600

    View and search added to test.

--- web/app/tests/Http/Controller/CarControllerTest.php
+++ web/app/tests/Http/Controller/CarControllerTest.php
@@ -2,10 +2,15 @@
 
 namespace Http\Controller;
 
+use App\Exceptions\RecordNotFoundException;
 use App\Exceptions\ValidationExceptions;
+use App\Helpers\Arr;
+use App\Helpers\ConfigDatabase;
+use App\Helpers\DBCleanup;
 use App\Http\Controller\CarController;
 use App\Lib\Request;
 use App\Lib\Response;
+use App\Services\VehicleService;
 use Faker\Factory;
 use Faker\Generator;
 use PHPUnit\Framework\TestCase;
@@ -26,6 +31,11 @@ class CarControllerTest extends TestCase
     {
         $this->faker = null;
         $this->controller = null;
+        DBCleanup::cleanCarFeatures();
+        DBCleanup::cleanVehicles();
+        DBCleanup::cleanbrands();
+        DBCleanup::cleanCarModels();
+        DBCleanup::cleanLocations();
     }
 
     public function testSaveWithInvalidData()
@@ -89,5 +99,74 @@ class CarControllerTest extends TestCase
             ->with(true);
 
         $this->assertNull($this->controller->save($request, $response));
+    }
+
+    /**
+     * @return void
+     * @throws RecordNotFoundException
+     * @throws \JsonException
+     */
+    public function testView(): void
+    {
+        $vehicleId = ConfigDatabase::setupVehicle();
+        $request = $this->createMock(Request::class);
+        $response = $this->createMock(Response::class);
+        $this->assertNull($this->controller->view($request, $response, $vehicleId));
+    }
+
+    /**
+     * @return void
+     * @throws RecordNotFoundException
+     */
+    public function testViewException(): void
+    {
+        $request = $this->createMock(Request::class);
+        $response = $this->createMock(Response::class);
+        $this->expectException(RecordNotFoundException::class);
+        $this->controller->view($request, $response, -1);
+    }
+
+    public function testIndex()
+    {
+        $input = $this->rawUserInput();
+        (new VehicleService())->storeCar($input);
+        $request = $this->createMock(Request::class);
+        $request->expects($this->once())
+            ->method('getParams')
+            ->willReturn(
+                Arr::only($input, [
+                    'car_model',
+                    'car_brand',
+                    'location',
+                    'license_plate',
+                    'fuel_type',
+                    'transmission',
+                    'car_type'
+                ])
+            );
+
+        $response = $this->createMock(Response::class);
+        $this->assertNull($this->controller->index($request, $response));
+    }
+
+    /**
+     * @return array
+     */
+    private function rawUserInput(): array
+    {
+        return [
+            'location' => $this->faker->realTextBetween(6, 10) . time(),
+            'car_brand' => $this->faker->realTextBetween(6, 10) . time(),
+            'car_model' => $this->faker->realTextBetween(6, 10) . time(),
+            'license_plate' => $this->faker->unique()->text(6) . '-' . time(),
+            'car_year' => $this->faker->year(),
+            'number_of_doors' => $this->faker->randomDigit(),
+            'number_of_seats' => $this->faker->randomDigit(),
+            'fuel_type' => $this->faker->realTextBetween(6, 10),
+            'transmission' => $this->faker->realTextBetween(6, 10),
+            'car_type_group' => $this->faker->realTextBetween(6, 10),
+            'car_type' => $this->faker->realTextBetween(6, 10),
+            'car_km' => $this->faker->numberBetween(0, 20000)
+        ];
     }
 }
